4f69bacfd15d5ea9baf9e26c4695ffe59c3cfcac
deniskeller <dentean@mail.ru>
2020-05-15 22:45:44 +0400

    finish base function

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,38 @@
+import {
+  $
+} from "@core/dom"
+
+export class Excel {
+  constructor(selector, options) {
+    this.$el = $(selector)
+    this.components = options.components || []
+  }
+
+  getRoot() {
+    const $root = $.create('div', 'excel')
+    // const $root = document.createElement('div')
+    // $root.classList.add('excel')
+
+    this.components = this.components.map(Component => {
+      // const $el = document.createElement('div')
+      // $el.classList.add(Component.className)
+      const $el = $.create('div', Component.className)
+      const component = new Component($el)
+      // DEBUG
+      if (component.name) {
+        window['c' + component.name] = component
+      }
+      // $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
+      // debugger
+      $root.append($el)
+      return component
+    })
+    return $root
+  }
+
+  render() {
+    this.$el.append(this.getRoot())
+    this.components.forEach(component => component.init())
+  }
+}
